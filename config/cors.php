495f18718b0922db7010dd8c662769bfb83b70f6
Busetdah <apriantowpj@gmail.com>
2025-04-26 20:49:28 +0700

    new cors

--- config/cors.php
+++ config/cors.php
@@ -14,8 +14,7 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['GET'],
+    'paths' => ['*', 'sanctum/csrf-cookie'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
